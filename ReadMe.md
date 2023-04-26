751991070e54b7e932a3ab375960aded4a57a35a
john4suresh <johnsuresh777@gmail.com>
2023-04-26 23:45:44 +0530

    Initial Routing and Nested Routes

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+## Introduction to React Router
+
+
+## Nested Routes
---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+import "../server";
+import Vans from "./pages/Vans/Vans";
+import VanDetail from "./pages/Vans/VanDetail";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Host/Dashboard";
+import HostLayout from "./components/HostLayout";
+import Income from "./pages/Host/Income";
+import Reviews from "./pages/Host/Review";
+import HostVans from "./pages/Host/Hostvans";
+import HostVanDetail from "./pages/Host/HostVanDetail";
+import HostVanInfo from "./pages/Host/HostVanInfo";
+import HostVanPhotos from "./pages/Host/HostVanPhotos";
+import HostVanPricing from "./pages/Host/HostVanPricing";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+              <Route />
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
